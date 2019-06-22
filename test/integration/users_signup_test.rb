2b7e0232e5023437d4ec6c78d7c8dfaca54e6561
pepechoko <hitoshi.t.pepe3@gmail.com>
2019-06-22 10:40:46 +0900

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,17 +2,30 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  test "invalid signup infomation" do
+  test "valid signup infomation" do
     get signup_path
-    assert_no_difference 'User.count' do
-      post users_path, params: { user: { name: "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
-      assert_template 'users/new'
-      assert_select 'div#<css id for error explanation>'
-      assert_select 'div.<css id for error explanation>'
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example Usr",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
     end
-
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
   end
+
+  # test "invalid signup infomation" do
+  #   get signup_path
+  #   assert_no_difference 'User.count' do
+  #     post users_path, params: { user: { name: "",
+  #                                        email: "user@invalid",
+  #                                        password:              "foo",
+  #                                        password_confirmation: "bar" } }
+  #     assert_template 'users/new'
+  #     assert_select 'div#<css id for error explanation>'
+  #     assert_select 'div.<css id for error explanation>'
+  #   end
+
+  # end
 end
