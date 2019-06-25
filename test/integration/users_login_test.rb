59c431f70c8144e45d4dfdb877a4e45b0400c33b
pepechoko <hitoshi.t.pepe3@gmail.com>
2019-06-26 02:11:38 +0900

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -8,9 +8,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with invalid infomation" do
     get login_path
-    assert_template 'session/new'
+    assert_template 'sessions/new'
     post login_path, params: { session: { email: "", password: "" }}
-    assert_template 'session/new'
+    assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
@@ -30,9 +30,25 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # ２番目のウィンドウでログアウトをクリックするユーザーをシュミレートする
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+
+  test "login without remenbering" do
+    # クッキーを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    # クッキーを削除してログイン
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
   end
 end
