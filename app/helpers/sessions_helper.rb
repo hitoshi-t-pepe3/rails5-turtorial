f527947fb5a74af89483c82ef1f8d0f59edd903e
pepechoko <hitoshi.t.pepe3@gmail.com>
2019-07-02 21:01:09 +0900

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,19 +5,6 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # 記憶トークンcookieに対応するユーザーを返す
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: session[:user_id])
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
-  end
-
   # ユーザーがログインしていればtrue、その他ならfalse
   def logged_in?
     !current_user.nil?
@@ -36,6 +23,24 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+
+  # 記憶トークンcookieに対応するユーザーを返す
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
   # 永続的セッションを破棄する
   def forget(user)
     user.forget
@@ -49,4 +54,16 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  # 記録したURL（もしくはデフォルト値）にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+  
 end
